94feb848ecf7f3fb69ecb032080dd0bcfbbe6476
scorcism <abhishekpathak1720@gmail.com>
2022-06-06 09:52:04 +0530

    cors fix

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const path = require('path');
 var cors = require('cors')
 const app = express()
 const port = process.env.PORT ||  5000
-app.use(cors())
+app.use(cors({
+  origin: "*",
+}))
 app.use(express.json()) // this is used to parse all the incoming data 
 const db = require('./db')
 
